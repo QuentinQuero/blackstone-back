aa3e4c9571b1d7eb307453bc9159a45a9fb7cab7
Renard <mathis.renard@epitech.eu>
2021-01-21 11:29:29 +0100

    Modify function createCharacter and updateCharacter/ Modify CharacterModel for exalte

--- services/character.service.js
+++ services/character.service.js
@@ -13,34 +13,7 @@ exports.getCharacter = async function () {
 exports.createCharacter = async function (character) {
     try {
         let newChar = new Character();
-
-        var array = character.capacity;
-        var array1 = character.specialRole;
-        var array2 = character.exalte;
-
-        newChar.name = character.name;
-        newChar.life = character.life;
-        newChar.movement = character.movement;
-        newChar.defense = character.defense;
-        newChar.agility = character.agility;
-        newChar.vitality = character.vitality;
-
-        array.forEach(element =>
-        newChar.capacity.push(element)
-        );
-
-        newChar.specialAttack = character.specialAttack;
-
-        array1.forEach(element =>
-        newChar.specialRole.push(element)
-        );
-
-        array2.forEach(element =>
-        newChar.exalte.push(element)
-        );
-
-        newChar.image = character.image;
-        newChar.vaisseau_id = character.vaisseau_id;
+        Object.assign(newChar,character);
         return await newChar.save();
     } catch (e) {
         // Log Errors
@@ -65,22 +38,12 @@ exports.deleteCharacter = async function (id) {
         throw Error('Error while Paginating Character')
     }
 }
-/*
-exports.updateCharacter = async function (character) {
+
+exports.updateCharacter = async function (character,id) {
     try {
-        let updateCharacter = await Character.findById(character._id);
-        updateCharacter.name = character.name;
-        updateCharacter.life = character.life;
-        updateCharacter.capacity.attack = character.capacity.attack;
-        updateCharacter.specialAttack = character.specialAttack;
-        updateCharacter.specialRole.name = character.specialRole.name;
-        updateCharacter.specialRole.description =character.specialRole.description;
-        updateCharacter.exalte.possible = character.exalte.possible;
-        updateCharacter.exalte.description = character.exalte.description;
-        return Character.updateOne({_id : character._id}, updateCharacter);
+        return Character.updateOne({_id : id}, character);
     } catch (e) {
         // Log Errors
         throw Error('Error while Paginating Character')
     }
 }
-*/
