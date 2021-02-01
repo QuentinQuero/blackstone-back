35f22821cc91f6d49fbab696bbcab5673987b152
Roma <roman.fechtchenko@epitech.eu>
2021-02-01 11:03:22 +0100

    add exploration image

--- models/combatmapsModel.js
+++ models/combatmapsModel.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose')
 const CombatMapsSchema  = new mongoose.Schema({
     name: String,
     image: String,
-    hostileNumber: Number,
+    hostileNumber: Number
 })
 
 const Map = mongoose.model('maps', CombatMapsSchema)
